ce63d642bb96c645a60b38ef5f9a22aaf52335fe
Javier <javier_aml@outlook.com>
2022-11-26 08:38:52 -0600

    Commit inicial

---
+++ components/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="login">
+        <form>
+            <h3>Ingresar</h3>
+            <div class="form-group">
+                <label>Correo</label>
+                <input type="email" class="form-control form-control-lg" />
+            </div>
+            <div class="form-group">
+                <label>Contraseña</label>
+                <input type="password" class="form-control form-control-lg" />
+            </div>
+            <button type="submit" class="btn btn-dark btn-lg btn-block">Ingresar</button>
+            <p class="text-right mt-2 mb-4">Registrarse</p>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {}
+        }
+    }
+</script>
+
+<style scoped>
+.login{
+    width: 20%;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <login class="login"></login>
+</template>
+
+<script>
+import Login from '../components/Login.vue'
+
+export default {
+  components: { Login }
+}
+</script>
+
+<style scoped>
+.login{
+  margin-top: 50px;
+}
+</style>
